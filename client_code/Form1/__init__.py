6865e12d047a19662d8aa1ce931c76304eb4e001
krishnakarthik967@gmail.com <krishnakarthik967@gmail.com>
2026-06-07 03:52:27 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +12,8 @@ class Form1(Form1Template):
     super().__init__(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("outlined_button_1", "click")
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass  # Write Code Here
